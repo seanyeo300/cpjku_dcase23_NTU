d533ec57a91e2b33784929deb7f9ac07e4258807
seanyeo300 <seanyeo300@gmail.com>
2024-08-17 22:50:46 +0800

    Update multi_run_eval.py

--- multi_run_eval.py
+++ multi_run_eval.py
@@ -11,9 +11,9 @@ def run_multiple_scripts(scripts_with_args):
 if __name__ == "__main__":
     # Example: Replace these with your actual script names and arguments
     scripts_to_run = [
-        ('run_passt_training_subsets_DIR_FMS_h5.py', ['--evaluate', '--ckpt_id', 'asvkheqw'])
-        # ('run_passt_training_subsets_DIR_FMS.py', ['--evaluate', '--ckpt_id', 'rk677ntr'])
-        # ('run_passt_training_subsets_DIR_FMS.py', ['--evaluate', '--ckpt_id', 'xa9sscwh'])
+        ('run_passt_training_subsets_DIR_FMS_h5.py', ['--evaluate', '--ckpt_id', 'asvkheqw']),
+        ('run_passt_training_subsets_DIR_FMS.py', ['--evaluate', '--ckpt_id', 'wai9pzst']),
+        ('run_passt_training_subsets_DIR_FMS.py', ['--evaluate', '--ckpt_id', 'anijixsa'])
     ]
     run_multiple_scripts(scripts_to_run)
 #25%
