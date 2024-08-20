9fe0f414e83c96b96e6b608bb9e4bfde0c80c261
seanyeo300 <seanyeo300@gmail.com>
2024-08-20 16:16:18 +0800

    Update run_passt_cochl_PT_mel_h5_slowfast.py

--- run_passt_cochl_PT_mel_h5_slowfast.py
+++ run_passt_cochl_PT_mel_h5_slowfast.py
@@ -522,7 +522,7 @@ if __name__ == '__main__':
 
     # general
     parser.add_argument('--project_name', type=str, default="NTU24_ASC")
-    parser.add_argument('--experiment_name', type=str, default="CPJKU_passt_cochl_PT_mel_10s_h5")
+    parser.add_argument('--experiment_name', type=str, default="CPJKU_passt_cochl_slow_fast_h5")
     parser.add_argument('--num_workers', type=int, default=0)  # number of workers for dataloaders
     parser.add_argument('--precision', type=str, default="32")
     
@@ -533,7 +533,7 @@ if __name__ == '__main__':
     # dataset
     # location to store resampled waveform
     parser.add_argument('--cache_path', type=str, default=os.path.join("datasets", "cpath"))
-    parser.add_argument('--subset', default="cochl")
+    parser.add_argument('--subset', default="cochl10s")
     # model
     parser.add_argument('--arch', type=str, default='passt_s_swa_p16_128_ap476')  # pretrained passt model
     parser.add_argument('--n_classes', type=int, default=13)  # classification model with 'n_classes' output neurons
