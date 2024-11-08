ee73795d14f6222dbfacb2e51c18f7f032618644
seanyeo300 <seanyeo300@gmail.com>
2024-11-08 16:13:31 +0800

    Update multi_run_single.py

--- multi_run_single.py
+++ multi_run_single.py
@@ -34,25 +34,32 @@ def run_multiple_scripts(script_name, base_args, ckpt_experiment_pairs, num_repe
 
 if __name__ == "__main__":
     # Define the script to run
-    # script_name = 'run_passt_cochl_FT_subsets_DIR_FMS_h5.py'
+    # script_name = 'run_training_dev_h5.py'
     script_name = 'run_passt_cochl_tau_slowfast_subsets_DIR_FMS_h5.py'
+    # script_name = 'run_passt_cochl_PT_FT_tau_subsets_h5.py'
+    # script_name = 'run_training_DynMN_h5_PL.py'
     # Base arguments (common to all runs, except experiment name and ckpt_id)
-    base_args = ["--subset", "100", "--dir_prob", "0.6", "--mixstyle_p", "0.4", ] #"--timem" , "0" check which script you are running. If you want to use SL or FT, switch script as needed.
-     
+    base_args = ['--gpu','[0]',"--subset", "50", "--dir_prob", "0.6", "--mixstyle_p", "0.4","--n_epochs", "25","--n_classes","13"]
+    
     # List of tuples containing checkpoint IDs and their corresponding experiment names
     ckpt_experiment_pairs = [
 
-        # ("utupypwc", "NTU_PaSST_FTcs_FTtau_wk50wxro_sub25_FMS_DIR_fixh5"),       #SeqFT 1e-4 
-        # ("0r39k52v", "NTU_PaSST_FTcs_FTtau_0r39k52v_sub25_FMS_DIR_fixh5"),          #SeqFT 1e-5 
-        # ("6ip7syrn", "NTU_PaSST_FTcs_FTtau_7qghtor2_sub25_FMS_DIR_fixh5"),       #SeqFT 1e-6
+        # ("utupypwc", "NTU_PaSST_FTcs_FTtau_utupypwc_sub25_FMS_DIR_fixh5"),       #SeqFT 1e-4 
+        # ("0r39k52v", "NTU_PaSST_FTcs_FTtau_0r39k52v_sub100_FMS_DIR_fixh5"),       #SeqFT 1e-5 
+        # ("6ip7syrn", "NTU_PaSST_FTcs_FTtau_6ip7syrn_sub25_FMS_DIR_fixh5"),       #SeqFT 1e-6
         # ("wk50wxro", "NTU_PaSST_SLcs_FTtau_wk50wxro_sub25_FMS_DIR_fixh5"),       #SL 1e-4 SIT
-        # ("qrrag30b", "NTU_PaSST_SLcs_FTtau_qrrag30b_sub25_FMS_DIR_fixh5"),          #SL 1e-5 SIT
+        # ("qrrag30b", "NTU_PaSST_SLcs_FTtau_qrrag30b_sub25_FMS_DIR_fixh5"),       #SL 1e-5 SIT
         # ("7qghtor2", "NTU_PaSST_SLcs_FTtau_7qghtor2_sub25_FMS_DIR_fixh5")        #SL 1e-6 SIT 
-        ("wk50wxro", "NTU_PaSST_SLcs_SLtau_wk50wxro_sub100_FMS_DIR_fixh5"),       #SL 1e-4 DSIT (half complete, see
-                                                                                   #NTU_passt_SLcs_SLtau_wk50wxro_sub10_FMS_DIR_PretrainedStudent_fixh5)
+        # ("wk50wxro", "NTU_PaSST_SLcs_SLtau_wk50wxro_sub5_DIR_fixh5"),             #SL 1e-4 DSIT 
         # ("qrrag30b", "NTU_PaSST_SLcs_SLtau_qrrag30b_sub25_FMS_DIR_fixh5"),       #SL 1e-5 DSIT
         # ("7qghtor2", "NTU_PaSST_SLcs_SLtau_7qghtor2_sub25_FMS_DIR_fixh5")        #SL 1e-6 DSIT
-        # (None, "NTU_PaSST_FTtau_sub100_FMS_DIR_fixh5")
+        # (None, "sBCBL_FTtau_sub10_FMS_DIR_fixh5")                              #PTau First model trained used mixup, have disabled default mixup for the next 5
+        # ("g1gzf3te", "NTU_PaSST_SLcsOL_SLtau_g1gzf3te_sub5_FMS_DIR_50epoch_fixh5"),       #SL 1e-4 DSIT
+        # ("v7q0fght", "NTU_PaSST_SLcsOL_SLtau_v7q0fght_sub5_FMS_DIR_fixh5"),
+        # ("j98ea5ub", "NTU_PaSST_SLcsNoOL_SLtau_j98ea5ub_sub5_FMS_DIR_fixh5"),       #SL 1e-4 DSIT 
+        ("nmioakwl", "NTU_PaSST_SLcs_sub10_SLtau_nmioakwl_sub50_FMS_DIR_fixh5"),       #SL 1e-4 DSIT 
+        
+        # (None, "tMN30_FTtau_32K_FMS_DIR_sub5_fixh5")                       #FTtau_noASpt 
 
     ] 
     
