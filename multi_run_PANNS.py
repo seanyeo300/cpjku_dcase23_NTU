d2a49c89b30cbaa076cec0bba1972fdc2d5c0912
seanyeo300 <seanyeo300@gmail.com>
2025-04-03 10:12:15 +0800

    Update multi_run_PANNS.py

--- multi_run_PANNS.py
+++ multi_run_PANNS.py
@@ -1,44 +1,4 @@
 # multi_run.py
-import subprocess
-
-# def run_multiple_scripts(scripts_with_args):
-#     try:
-#         for script_name, args in scripts_with_args:
-#             subprocess.run(['python', script_name] + args)
-#     except Exception as e:
-#         print(f"An error occurred: {e}")
-
-# if __name__ == "__main__":
-#     # Example: Replace these with your actual script names and arguments
-#     scripts_to_run = [
-#         ('run_passt_cochl_PT_mel_h5.py', ['--lr', 1e-4]),
-#         ('run_passt_cochl_PT_mel_h5.py', [ '--lr', 1e-5]),
-#         ('run_passt_cochl_PT_mel_h5.py', [ '--lr', 1e-6])
-#     ]
-#     run_multiple_scripts(scripts_to_run)
-    
-#     import subprocess
-
-# def run_multiple_scripts(scripts_with_args):
-#     try:
-#         for script_name, args in scripts_with_args:
-#             # Convert all arguments to strings
-#             args = [str(arg) for arg in args]
-#             subprocess.run(['python', script_name] + args)
-#     except Exception as e:
-#         print(f"An error occurred: {e}")
-
-# if __name__ == "__main__":
-#     # Example: Replace these with your actual script names and arguments
-#     scripts_to_run = [
-#         ('run_passt_KD_Cochl_TAU_FT_subsets_DIR_FMS_h5.py', [ "--subset", "5", "--dir_prob", "0.6","--ckpt_id", "zs2yso3b", "--experiment_name", "NTU_KD_Var2b-T_SIT-S_FMS_DIR_sub5_fixh5"]),
-#         ('run_passt_KD_Cochl_TAU_FT_subsets_DIR_FMS_h5.py', [ "--subset", "5", "--dir_prob", "0.6","--ckpt_id", "zs2yso3b", "--experiment_name", "NTU_KD_Var2b-T_SIT-S_FMS_DIR_sub5_fixh5"]),
-#         ('run_passt_KD_Cochl_TAU_FT_subsets_DIR_FMS_h5.py', [ "--subset", "5", "--dir_prob", "0.6","--ckpt_id", "zs2yso3b", "--experiment_name", "NTU_KD_Var2b-T_SIT-S_FMS_DIR_sub5_fixh5"]),
-#         ('run_passt_KD_Cochl_TAU_FT_subsets_DIR_FMS_h5.py', [ "--subset", "5", "--dir_prob", "0.6","--ckpt_id", "zs2yso3b", "--experiment_name", "NTU_KD_Var2b-T_SIT-S_FMS_DIR_sub5_fixh5"]),
-#         ('run_passt_KD_Cochl_TAU_FT_subsets_DIR_FMS_h5.py', [ "--subset", "5", "--dir_prob", "0.6","--ckpt_id", "zs2yso3b", "--experiment_name", "NTU_KD_Var2b-T_SIT-S_FMS_DIR_sub5_fixh5"]),
-#         ('run_passt_KD_Cochl_TAU_FT_subsets_DIR_FMS_h5.py', [ "--subset", "5", "--dir_prob", "0.6","--ckpt_id", "zs2yso3b", "--experiment_name", "NTU_KD_Var2b-T_SIT-S_FMS_DIR_sub5_fixh5"])
-#     ]
-#     run_multiple_scripts(scripts_to_run)
     
 import subprocess
 
@@ -60,7 +20,7 @@ if __name__ == "__main__":
     script_name = 'run_PANN_training_subsets_DIR_FMS_h5.py'
     
     # Base arguments (common to all runs, except experiment name and ckpt_id)
-    base_args = ["--subset", "5", "--n_epochs", "12" ,"--dir_prob", "0.6", "--mixstyle_p", "0.4", "--weight_decay", "0.001", "--lr", "1e-4", "--resample_rate", "44100"]
+    base_args = ["--subset", "5", "--gpu", "[1]" ,"--n_epochs", "12" ,"--dir_prob", "0.6", "--mixstyle_p", "0.4", "--weight_decay", "0.001", "--lr", "1e-4", "--resample_rate", "44100"]
     
     # List of tuples containing checkpoint IDs and their corresponding experiment names
     ckpt_experiment_pairs = [
@@ -69,7 +29,7 @@ if __name__ == "__main__":
         # ("dbl1yun4", "NTU_KD_Var1-T_SIT-S_FMS_sub5_fixh5"),      #SIT FMS
         # ("lm7o54or", "NTU_KD_Var1-T_SeqFT-S_FMS_DIR_sub5_fixh5"),#SeqFT
         # ("f5hhbj59", "NTU_KD_Var1-T_FTtau-S_FMS_DIR_sub5_fixh5"),#FTtau
-        (None, "NTU_PANNs_FTtau_441K_FMS_DIR_1e-4_adamw_WD0.001_h5")          #Ptau
+        (None, "NTU_PANNs_FTtau_441K_nofmin_FMS_DIR_1e-4_adamw_WD0.001_h5")          #Ptau
     ]
     
     # Number of times to repeat each experiment
